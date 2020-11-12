0dfa436b7b0c22d7c9a6797de78508d9d6f03629
Rishabh Mehan <darkrishabh@gmail.com>
2020-11-12 01:10:33 -0800

    remote additional meta tags for twitter

--- utils/seo.js
+++ utils/seo.js
@@ -11,26 +11,7 @@ export const SEO = {
         description: SiteDescription,
         images: [{ url: SiteConfig.image }],
     },
-    additionalMetaTags: [
-        {
-            name: 'twitter:url',
-            property: SiteConfig.url,
-        },
-        {
-            name: 'twitter:description',
-            property: SiteConfig.description,
-        },
-        {
-            name: 'twitter:title',
-            property: SiteConfig.title,
-        },
-        {
-            name: 'twitter:card',
-            property: `summary_large_image`,
-        },
-        {
-            name: 'twitter:image',
-            property: SiteConfig.image,
-        },
-    ],
+    twitter: {
+        cardType: 'summary_large_image',
+    },
 }
